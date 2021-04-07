725c63e0299f7c63aba8f9ccf0644732d77d80b4
Victor An <67305706+victoran0508@users.noreply.github.com>
2021-04-08 07:17:09 +0900

    0408

---
+++ assets/js/theme.js
@@ -0,0 +1,47 @@
+jQuery(function($){
+	$(document).ready(function(){
+		$('.hero-carousel').slick({
+			lazyLoad: 'ondemand',
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 6000,
+			infinite: true,
+			speed: 1000,
+			fade: true,
+			cssEase: 'linear'
+		});
+
+
+		if ($(window).scrollTop() >= 100) {
+			$('.page-header').addClass('header-sticky');
+		} else {
+			$('.page-header').removeClass('header-sticky');
+		}
+	});
+
+	$(window).scroll(function(){
+		if ($(window).scrollTop() >= 100) {
+			$('.page-header').addClass('header-sticky');
+		} else {
+			$('.page-header').removeClass('header-sticky');
+		}
+	});
+
+	$('.nav-open').off('click').on('click', function(){
+		if($('body').hasClass('nav-opened'))
+			$('body').removeClass('nav-opened');
+		else
+			$('body').addClass('nav-opened');
+	});
+
+	$('.submenu-opener').off('click').on('click', function(){
+		if(!$(this).hasClass('opened')) {
+			$(this).next('.menu-popup').slideDown();
+			$(this).addClass('opened');
+		} else {
+			$(this).next('.menu-popup').slideUp();
+			$(this).removeClass('opened');
+		}
+	});
+});
