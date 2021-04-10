6362e3bbe793ecea278c0b250f8555b13abf5c7e
Victor An <67305706+victoran0508@users.noreply.github.com>
2021-04-10 12:55:47 +0900

    added 2 pages

---
+++ header-white.php
@@ -0,0 +1,37 @@
+<!-- HTMLコード -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Script-Type" content="text/javascript" />
+	<meta http-equiv="Content-Style-Type" content="text/css" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+
+    <!-- css -->
+    <link rel="stylesheet" href="./libs/slick/slick.css">
+    <link rel="stylesheet" href="./libs/slick/slick-theme.css">
+    <link rel="stylesheet" href="./assets/css/theme.css">
+
+    <!-- javascript -->
+    <script src="./libs/jquery/jquery-3.4.1.min.js"></script>
+</head>
+
+<body>
+    <header class="page-header page-header--white-background">
+        <div class="header-inner">
+            <a href="#" class="logo"><img src="./assets/img/logo.png" alt="" /></a>
+            <a href="javascript:void(0)" class="nav-open"><i></i><span></span></a>
+            <div class="nav-wrapper">
+                <ul class="nav">
+                    <li><a href="/concept">Concept</a></li>
+                    <li><a href="/arrange">Arrange</a></li>
+                    <li><a href="/bridal">Bridal</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/news">News</a></li>
+                    <li><a href="/contact" class="contact"><img class="img" src="./assets/img/envelope.svg" alt="" /></a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
